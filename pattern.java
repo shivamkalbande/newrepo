6626df54520342acb37af3846631670ca911abd2
shivam <shivam.kalbande23@gmail.com>
2021-11-16 16:20:06 +0530

    v1

---
+++ pattern.java
@@ -0,0 +1,16 @@
+//package testJavaProject;
+import java.util.*;
+
+public class pattern {
+	public static void main(String [] args) {
+		int i=0;
+		for(i=0 ; i<10; i++) {
+			for(int j=i; j>=0; j--) {
+
+				System.out.print("*");
+			}
+			System.out.println(+(i+1)+"\n");
+		}
+	}
+
+}
